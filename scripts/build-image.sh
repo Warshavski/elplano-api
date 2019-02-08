92c938c3d3f3e12dd48d022e986faeb011180428
warshavski <p.warshavski@gmail.com>
2019-02-08 22:57:53 +0300

    Fixed: please docker please!

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,7 +2,7 @@
 set -e
 set -v
 
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker pull ${DOCKER_REPOSITORY}:last_successful_build || true
 docker pull ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} || true
@@ -11,7 +11,7 @@ docker build -t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} --pull=true .
 
 docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
 
-docker tag -f ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}:last_successful_build
+docker tag ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}:last_successful_build
 
 docker push ${DOCKER_REPOSITORY}:last_successful_build
 

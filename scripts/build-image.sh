723045fe071b955b672d132767f82325f52825e0
warshavski <p.warshavski@gmail.com>
2019-02-14 15:28:00 +0300

    Fixed: renamed last successful docker build tag

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,14 +4,14 @@ set -v
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker pull ${DOCKER_REPOSITORY}:last_successful_build || true
+docker pull ${DOCKER_REPOSITORY}:latest || true
 docker pull ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} || true
 
 docker build -t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} --pull=true .
 
 docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
 
-docker tag ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}:last_successful_build
+docker tag ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} ${DOCKER_REPOSITORY}:latest
 
-docker push ${DOCKER_REPOSITORY}:last_successful_build
+docker push ${DOCKER_REPOSITORY}:latest
 

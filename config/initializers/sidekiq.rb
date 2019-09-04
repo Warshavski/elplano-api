862d26fb8c1b2ea5742fa1aa553d5e9b94c3ea82
warshavski <p.warshavski@gmail.com>
2019-09-04 09:45:35 +0300

    Closes #153 Add admin user
    Added admin user creation from DB seed

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -31,3 +31,18 @@ end
 Sidekiq.configure_client do |config|
   config.redis = queues_config_hash
 end
+
+if Rails.env.production?
+  Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
+    #
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking
+    #     (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    #
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['ADMIN_EMAIL'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
+  end
+end

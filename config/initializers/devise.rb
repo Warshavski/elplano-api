1955474fb77f71efcd90d714ff6fb6c09f851b6c
warshavski <p.warshavski@gmail.com>
2020-02-17 10:05:42 +0300

    Change mailer setup for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,10 +21,10 @@ Devise.setup do |config|
   config.mailer_sender = ENV['MAILER_FROM']
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'DeviseMailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ApplicationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
